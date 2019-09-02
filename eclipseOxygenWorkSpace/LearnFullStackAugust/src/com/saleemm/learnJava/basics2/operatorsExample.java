a113f9d6df90d74c546018220a18aa1a0f1f07dc
Saleem Mohammad <zaahir.saleem9@gmail.com>
2019-09-02 19:36:11 -0400

    Added OperatorsExample

---
+++ eclipseOxygenWorkSpace/LearnFullStackAugust/src/com/saleemm/learnJava/basics2/operatorsExample.java
@@ -0,0 +1,17 @@
+package com.saleemm.learnJava.basics2;
+
+public class operatorsExample {
+
+	public static void main(String[] args) {
+		// check a condition
+		int age = 18;
+		if (age > 21) {
+			System.out.println("Age is greater than 21 :: " + age);
+		}else {
+			System.out.println("Age is less than 21 :: " + age);
+
+		}
+
+	}
+
+}
