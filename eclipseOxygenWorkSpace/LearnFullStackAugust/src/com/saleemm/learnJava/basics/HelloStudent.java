dfdd940947ca7ca66f75005939e77b39d47c014e
Saleem Mohammad <zaahir.saleem9@gmail.com>
2019-09-02 16:50:59 -0400

    Learning Variable and Methods

---
+++ eclipseOxygenWorkSpace/LearnFullStackAugust/src/com/saleemm/learnJava/basics/HelloStudent.java
@@ -0,0 +1,39 @@
+package com.saleemm.learnJava.basics;
+//Creating project
+//github repository
+//Package
+//creating a class
+
+
+public class HelloStudent {
+	
+	//vaiables ==> Data Carriers
+	//Data Types
+	//Eight primitive data types
+	//numeric data
+	
+	byte byteData = 99;
+	short shortData = 9999;
+	int intData = 999999999;
+	long longData = 9999999999999L; //Only here java is not case sensitive. L or l
+	//Decimals
+	float floatData = 99.99f;
+	double doubleData = 999.9999;
+	//Character Data
+	char charData = 'A'; //single quotes and single character
+	//Boolean
+	boolean boolData = false;
+	
+	//Non Premitive,  but used like a primitive
+	String strData  = "This is a String. From a single character to a book"; 
+
+	//methods ==> Data processors
+	public static void main (String[] args ) {
+		System.out.println("Hello World!!");
+		System.out.println("Remember control + space in Eclipse");
+		//System.out.println("testVar : " + testVar);
+		
+	}
+	
+
+}
