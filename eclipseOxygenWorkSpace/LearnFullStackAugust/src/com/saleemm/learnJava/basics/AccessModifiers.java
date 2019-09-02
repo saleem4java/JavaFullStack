c9899df1fb446adf4fdf6b2c2e3f402d7f97c365
Saleem Mohammad <zaahir.saleem9@gmail.com>
2019-09-02 15:21:37 -0400

    Learning Java

---
+++ eclipseOxygenWorkSpace/LearnFullStackAugust/src/com/saleemm/learnJava/basics/AccessModifiers.java
@@ -0,0 +1,13 @@
+package com.saleemm.learnJava.basics;
+
+public class AccessModifiers {
+	//Access Modifiers are applicable to classes, method, variables.
+	/**
+	 * public -- accessible to everyone
+	 * private -- accessible to only to declaration class.
+	 * protected -- ACC
+	 */
+public static void main(String[] args) {
+	
+}
+}
---
+++ eclipseOxygenWorkSpace/LearnFullStackAugust/src/com/saleemm/learnJava/basics2/Constructor.java
@@ -0,0 +1,19 @@
+package com.saleemm.learnJava.basics2;
+
+public class Constructor {
+
+	// 1. Dont have return type
+	// 2. Always have class names
+	// parameterized constructor-- const with arguments
+	
+	public Constructor ( ) {
+		System.out.println("Creating a instance of Constructor class");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Constructor constructor = new Constructor();
+
+	}
+
+}
